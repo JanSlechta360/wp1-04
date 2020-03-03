821e4f0e50dc0e2648a6890ef0cf132f112fb121
JanSlechta360 <60642873+JanSlechta360@users.noreply.github.com>
2020-03-03 14:20:22 +0100

    If age

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    $age 18;
+if ($age 18 >=18 {
+        $answer= Děkujeme za kšeft
+        $imgURL= <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ihlas.cz%2Foblibeny-alkohol-22%2F&psig=AOvVaw3HD6wMwr0rItdOxhMYZCe3&ust=1583310023893000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOD3y9Xv_ecCFQAAAAAdAAAAABAE"> 
+    } else {
+        $answer= Máš smůlu chlapče, alkohol a tabák prodáváme osobám starší 18 let.
+        $imgURL= <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fcs-cz.facebook.com%2Fradsejknihu%2F&psig=AOvVaw0LwkG0hUd692XF93imkPkN&ust=1583309639172000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMDx05vu_ecCFQAAAAAdAAAAABAa">
+    }
+    
+
+
+
+    
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+</head>
+      <body>
+           <?= $answer ?>
+           <img src="<?=$imgURL?">
+      </body>
+</html>
