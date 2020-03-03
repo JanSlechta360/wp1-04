17be14ccdc5d102b1a118657c2c43b3d53f87b0f
JanSlechta360 <60642873+JanSlechta360@users.noreply.github.com>
2020-03-03 14:21:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     $age 18;
-if ($age 18 >=18 {
+if ($age >= 18 {
         $answer= Děkujeme za kšeft
         $imgURL= <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ihlas.cz%2Foblibeny-alkohol-22%2F&psig=AOvVaw3HD6wMwr0rItdOxhMYZCe3&ust=1583310023893000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOD3y9Xv_ecCFQAAAAAdAAAAABAE"> 
     } else {
